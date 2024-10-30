036772435a105b2599ab287ca7ede2c035ec595e
daniilkimstach <daniilkimstachp@gmail.com>
2024-10-30 17:38:55 +0300

    4 lab

--- app/web/machine_translator.py
+++ app/web/machine_translator.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 
 from app.container import get_dependency
 from app.service.machine_translator import MachineTranslatorService
+from app.service.machine_translator.dto import MachineTranslatorRequest
 
 router = APIRouter(prefix="/machine-translator", tags=["machine-translator"])
 
@@ -10,9 +11,9 @@ router = APIRouter(prefix="/machine-translator", tags=["machine-translator"])
 @router.post("/translate")
 @inject
 async def translate(
-    text: str,
+    request: MachineTranslatorRequest,
     machine_translator: MachineTranslatorService = get_dependency(
         "machine_translator_service"
     ),
 ):
-    return machine_translator.getting_response_file(text)
+    return machine_translator.getting_response_file(request.text)
